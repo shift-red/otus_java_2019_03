efae079ccf799a15a8b61eb771484edb52e2ad82
Evgeniya Fedotova <shift-red@yandex-team.ru>
2019-04-02 23:55:35 +0300

    HW-1 add guava method call

---
+++ hw01-maven/src/main/java/ru/otus/shiftred/HelloOtus.java
@@ -0,0 +1,20 @@
+package ru.otus.shiftred;
+
+import com.google.common.base.Joiner;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloOtus {
+    private static final String HELLO = "Hello";
+    private static final String OTUS = "Otus";
+    private static final String EXCLAMATION_MARK = "!";
+
+    public static String guavaCall() {
+        return Joiner.on(" ").join(getStringListToJoin());
+    }
+
+    public static List<String> getStringListToJoin() {
+        return Arrays.asList(HELLO, OTUS, EXCLAMATION_MARK);
+    }
+}
